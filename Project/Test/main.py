df1dd1c7a3097047ced6f4db555c7a19b9ae56fe
Sofya <sofya.salmanova@playrix.com>
2018-09-12 23:22:36 +0300

    Web-server and Web-client: progress

--- Project/Test/main.py
+++ Project/Test/main.py
@@ -9,25 +9,28 @@ def run(server_class=http.server.HTTPServer,
 
 
 class HttpProcessor(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
+    def response_common_part(self, content_length=0):
+        self.protocol_version = "HTTP/1.1"
         self.send_response(200)
-        self.send_header('content-type', 'text/html')
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', str(content_length))
         self.end_headers()
-        self.wfile.write(b"<html><body><h1>I'm GET</h1></body></html>")
+
+    def do_GET(self):
+        body = b"<html><body><h1>I'm GET</h1></body></html>"
+        self.response_common_part(len(body))
+        self.wfile.write(body)
 
 
     def do_POST(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b"<html><body><h1>I'm POST</h1></body></html>")
+        body = b"<html><body><h1>I'm POST</h1></body></html>"
+        self.response_common_part(len(body))
+        self.wfile.write(body)
 
 
     def do_HEAD(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b"<html><body><h1>I'm HEAD</h1></body></html>")
+        self.response_common_part(0)
 
 
-run(handler_class=HttpProcessor)
+if __name__ == "__main__":
+    run(handler_class=HttpProcessor)
--- Project/WebClient/main.py
+++ Project/WebClient/main.py
@@ -1,13 +1,29 @@
 import requests
+import argparse
 
-r = requests.get('http://localhost:8000')
-print(r.content)
+parser = argparse.ArgumentParser(description='Connect to some Web-server.')
+parser.add_argument('method', default='get', help='supported methods: get, post, head')
+parser.add_argument('address', default='localhost', help='server address without http://')
+parser.add_argument('port', default='8000')
 
-r = requests.post('http://localhost:8000')
-print(r.content)
+args = parser.parse_args()
+url = 'http://' + args.address + ':' + args.port
+method = args.method.lower()
 
-r = requests.head('http://localhost:8000')
-print(r.content)
-print(r.headers)
+if method == 'get':
+    r = requests.get(url)
+elif method == 'post':
+    r = requests.post(url)
+elif method == 'head':
+    r = requests.head(url)
+else:
+    print('input method is not supported')
+
+if r.status_code == 200:
+    print(r.content)
+    print(r.headers)
+else:
+    print('Error:' + str(r.status_code))
+
 
 
