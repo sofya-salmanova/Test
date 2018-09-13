fcaff8269cbde6ffb23109a0510825457b54426c
Sofya <sofya.salmanova@playrix.com>
2018-09-13 13:47:00 +0300

    Web-server and Web-client: progress, part 2

--- Project/Test/main.py
+++ Project/Test/main.py
@@ -9,27 +9,28 @@ def run(server_class=http.server.HTTPServer,
 
 
 class HttpProcessor(http.server.BaseHTTPRequestHandler):
-    def response_common_part(self, content_length=0):
+    def response_common_part(self, code, message, body):
         self.protocol_version = "HTTP/1.1"
-        self.send_response(200)
+        self.send_response(code, message)
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', str(content_length))
+        self.send_header('Content-Length', str(len(body)))
         self.end_headers()
 
     def do_GET(self):
         body = b"<html><body><h1>I'm GET</h1></body></html>"
-        self.response_common_part(len(body))
+        self.response_common_part(200, 'OK', body)
         self.wfile.write(body)
 
 
     def do_POST(self):
         body = b"<html><body><h1>I'm POST</h1></body></html>"
-        self.response_common_part(len(body))
+        self.response_common_part(201, 'Created', body)
         self.wfile.write(body)
 
 
     def do_HEAD(self):
-        self.response_common_part(0)
+        body = b"<html><body><h1>I'm GET</h1></body></html>"
+        self.response_common_part(204, 'No Content', body)
 
 
 if __name__ == "__main__":
--- Project/WebClient/main.py
+++ Project/WebClient/main.py
@@ -1,29 +1,27 @@
 import requests
 import argparse
 
+functions = {'get': requests.get, 'post': requests.post, 'head': requests.head}
+
 parser = argparse.ArgumentParser(description='Connect to some Web-server.')
 parser.add_argument('method', default='get', help='supported methods: get, post, head')
+parser.add_argument('protocol', default='http://')
 parser.add_argument('address', default='localhost', help='server address without http://')
 parser.add_argument('port', default='8000')
 
 args = parser.parse_args()
-url = 'http://' + args.address + ':' + args.port
+url = args.protocol + args.address + ':' + args.port
 method = args.method.lower()
 
-if method == 'get':
-    r = requests.get(url)
-elif method == 'post':
-    r = requests.post(url)
-elif method == 'head':
-    r = requests.head(url)
-else:
-    print('input method is not supported')
 
-if r.status_code == 200:
+fmethod = functions.get(method)
+if fmethod != None:
+    r = fmethod(url)
     print(r.content)
     print(r.headers)
+
 else:
-    print('Error:' + str(r.status_code))
+    print('input method is not supported')
 
 
 
