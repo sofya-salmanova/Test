9d59535ae8701b802b546a750230a8aad2914c5e
Sofya <sofya.salmanova@playrix.com>
2018-09-13 22:09:46 +0300

    Web-server and Web-client: progress, part 4

--- Project/WebClient/main.py
+++ Project/WebClient/main.py
@@ -1,8 +1,6 @@
 import requests
 import argparse
 
-functions = {'get': requests.get, 'post': requests.post, 'head': requests.head}
-
 parser = argparse.ArgumentParser(description='Connect to some Web-server.')
 parser.add_argument('method', default='get', help='supported methods: get, post, head')
 parser.add_argument('protocol', default='http://')
@@ -15,15 +13,15 @@ method = args.method.lower()
 
 
 try:
-    fmethod = functions[method]
-except KeyError:
+    r = getattr(requests, method)(url)
+except AttributeError:
     print('input method %s is not supported' % method)
     raise SystemExit()
 
-r = fmethod(url)
 print(r.status_code)
-print(r.content)
 print(r.headers)
+print(r.content)
+
 
 
 
