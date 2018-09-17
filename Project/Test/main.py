741575ecd21427623576ebc37266bc2578d57cc4
Sofya <sofya.salmanova@playrix.com>
2018-09-17 22:21:18 +0300

    Web-server and Web-client: progress, part 6

--- Project/Test/main.py
+++ Project/Test/main.py
@@ -1,22 +1,31 @@
+import sys
+import os
 from http import server, HTTPStatus
+import click
+import magic
 
+@click.command()
+@click.option('--root-dir', default=os.getcwd() + '/Files')
+@click.option('--port', default=8000)
+def run(root_dir, port):
+    global root_directory
+    root_directory = root_dir
 
-def run(server_class=server.HTTPServer,
-        handler_class=server.BaseHTTPRequestHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
+    server_address = ('', port)
+    httpd = server.HTTPServer(server_address, HttpProcessor)
     httpd.serve_forever()
-
 
 class HttpProcessor(server.BaseHTTPRequestHandler):
     MIME_TYPE_PLAIN = 'plain/text'
     MIME_TYPE_HTML = 'text/html'
+
 
     def response_common_part(self, status, body=None, content_type=None, proto_ver="1.1"):
         content_type = content_type or self.MIME_TYPE_PLAIN
 
         self.protocol_version = f"HTTP/{proto_ver}"
         self.send_response(status)
+        self.send_header('User-Agent', 'SofyaWeb')
 
         if body is not None:
             self.send_header('Content-Type', content_type)
@@ -29,8 +38,24 @@ class HttpProcessor(server.BaseHTTPRequestHandler):
 
 
     def do_GET(self):
-        body = b"<html><body><h1>I'm GET</h1></body></html>"
-        self.response_common_part(HTTPStatus.OK, body, self.MIME_TYPE_HTML)
+
+        status = HTTPStatus.OK
+
+        if self.path == '/list':
+            body = ';'.join(os.listdir(root_directory)).encode()
+        elif self.path[:6] == '/file/':
+            try:
+                handle = open(root_directory + self.path[5:], 'rb')
+                body = handle.read()
+                handle.close()
+            except:
+                print(sys.exc_info()[0])
+                body = b"<html><body><h1>File not found</h1></body></html>"
+                status = HTTPStatus.NOT_FOUND
+        else:
+            body = b"<html><body><h1>I'm GET</h1></body></html>"
+
+        self.response_common_part(status, body, magic.from_buffer(body, mime = True))
 
 
     def do_POST(self):
@@ -44,4 +69,4 @@ class HttpProcessor(server.BaseHTTPRequestHandler):
 
 
 if __name__ == "__main__":
-    run(handler_class=HttpProcessor)
+    run()
--- Project/WebClient/main.py
+++ Project/WebClient/main.py
@@ -1,28 +1,86 @@
+import sys
 import requests
-import argparse
+import click
 
-parser = argparse.ArgumentParser(description='Connect to some Web-server.')
-parser.add_argument('method', default='get', help='supported methods: get, post, head')
-parser.add_argument('protocol', default='http://')
-parser.add_argument('address', default='localhost', help='server address without http://')
-parser.add_argument('port', default='8000')
+@click.command()
+@click.argument('address')
+@click.option('--protocol', default='http://')
+@click.option('--port', default='8000')
+def main_loop(protocol, address, port):
+    command_dict = {1:'get', 2:'post', 3:'head', 4:'list', 0:'exit'}
+    while True:
 
-args = parser.parse_args()
-url = args.protocol + args.address + ':' + args.port
-method = args.method.lower()
+        for key, value in command_dict.items():
+            print(f'[{key}] {value}')
+
+        while True:
+            try:
+                index = int(input())
+                if command_dict.get(index) != None:
+                    break
+            except:
+                continue
+
+        if index == 0:
+            break
+        elif 0 < index < 4:
+            connect(command_dict.get(index), protocol, address, port)
+        else:
+            connect('get', protocol, address, port, path = '/list')
 
 
-try:
-    r = getattr(requests, method)(url)
-except AttributeError:
-    print('input method %s is not supported' % method)
-    raise SystemExit()
+def connect(method, protocol, address, port, path = ''):
+    """
+    Connect to some Web-server
 
-print(r.status_code)
-print(r.headers)
-print(r.content)
+    address (server address without http://) \n
+    --protocol (default http://) \n
+    --port (default 8000)
+
+    """
+    url = protocol + address + ':' + port + path
+    method = method.lower()
 
 
+    try:
+        r = getattr(requests, method)(url)
+    except AttributeError:
+        print('input method %s is not supported' % method)
+        raise SystemExit()
+    except requests.exceptions.ConnectionError:
+        print("can't connect")
+        raise SystemExit()
+    except:
+        print(sys.exc_info()[0])
+        raise SystemExit()
+
+    print(r.status_code)
+    print(r.headers)
+
+    if path == '/list':
+
+        list = r.text.split(';')
+
+        count = 1
+        for file in list:
+            print(f'[{count}] {file}')
+            count += 1
+        print('[0] exit')
+
+        while True:
+            try:
+                index = int(input())
+                break
+            except:
+                continue
+
+        if index != 0:
+            connect(method, protocol, address, port, '/file/' + list[index - 1])
+    else:
+        print(r.content)
+
+if __name__ == '__main__':
+    main_loop()
 
 
 
